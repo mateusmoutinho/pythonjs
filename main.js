ae60d2f105148f40868a446e6506a67f81b6f332
Mateus Moutinho <mateusmoutinho01@gmail.com>
2020-10-25 07:49:55 -0300

    file partes done

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-
+const fs = require('fs')
 
 //-------------------- removing unhandling text_parts ----------------------------
 
@@ -128,4 +128,65 @@ function puts_bracelets_on_end_scope(string){
     
 }
 
-//------------------------------open and eddit file
+//------------------------------file dealing ------------------------------------
+
+function open_file(file_name){
+    let file = null
+
+try{
+    file  = fs.readFileSync(file_name,"utf8")
+    }
+    catch(e){
+        return false 
+    }
+
+    return file
+}
+
+function write_file(file_name,content){
+    fs.writeFile(file_name, content, function (err,data) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log(data);
+      });
+}
+
+function get_file_name_without_dot(file_name){
+    dot = file_name.indexOf(".")
+   file = file_name.substr(0,dot)
+   return file
+}
+//-------------------------------main code--------------------------------------
+
+function main(){
+
+file_name = process.argv[2]
+output    = process.argv[3]
+
+open_file = open_file(file_name)
+
+
+
+
+if(file_name == undefined){
+    console.log("no file for transpile")
+    return
+}
+else if(open_file == false){
+   console.log("cound't open the", file_name, "file")
+   return
+}
+
+console.log(output)
+
+//console.log(file)
+
+
+
+
+}
+
+write_file("teste","eai caralho")
+//main()
+
